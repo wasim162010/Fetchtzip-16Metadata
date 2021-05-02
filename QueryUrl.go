6b192d0b2e51ec3de56080abd5c018ea56e03980
Conduent <wasim.bari@conduent.com>
2021-05-02 21:41:10 +0530

    Removed unnecessary code

--- QueryUrl.go
+++ QueryUrl.go
@@ -52,7 +52,7 @@ go run QueryUrl.go  "sha256://0x7e99ecf3a4490e3044ccdf319898d77380a2fc20aae36b6e
 func main() {
 
 	urlObj := os.Args[1]
-	fmt.Println("url passed as command line argument ", urlObj)
+	//fmt.Println("url passed as command line argument ", urlObj)
 	u, err := url.Parse(urlObj)
 	if err != nil {
 		log.Fatalln(err)
@@ -114,10 +114,14 @@ func QuerySHA256Url(sha256pUrl string) (MetaData, error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fetchedURL := u.Path //     // "/https://storage.googleapis.com/tzip-16/taco-shop-metadata.json"
-	fmt.Println("split fetchedURL ", fetchedURL)
+	//  url.Path will fetch  http url : "/https://storage.googleapis.com/tzip-16/taco-shop-metadata.json"
+	fetchedURL := u.Path
+	//fmt.Println("split fetchedURL ", fetchedURL)
+
+	//Removing "/", prefixed to http url
 	fetchedURL = strings.Replace(fetchedURL, "/", "", 1)
-	fmt.Println("replace fetchedURL ", fetchedURL) // https://storage.googleapis.com/tzip-16/taco-shop-metadata.json
+
+	//fmt.Println("replace fetchedURL ", fetchedURL)
 
 	resp, err := http.Get(fetchedURL)
 	if err != nil {
@@ -131,20 +135,26 @@ func QuerySHA256Url(sha256pUrl string) (MetaData, error) {
 		return MetaData{}, err
 	}
 
+	// u.Host will give the hashed part of the url
 	fmt.Println("shA PART ", u.Host)
+
+	//fetching hash of response body and verifying it by comparing it with hash provided in url( extracted via u.Host)
 	valResult, err := fetchHashAndValidate(string(body), u.Host)
 	if err != nil {
-		fmt.Println("Issue while verifying hash ", err)
+		fmt.Println(valResult, err)
 		return MetaData{}, err
+	} else {
+		err1 := json.Unmarshal(body, &metadata)
+		if err1 != nil {
+			fmt.Println("Error while unmarshalling ", err)
+			return MetaData{}, err
+		}
+		fmt.Println("")
+		fmt.Println("Metadata")
+		fmt.Println(metadata)
+		return metadata, nil
 	}
-	fmt.Println("resphash value ", valResult)
 
-	if err != nil {
-		fmt.Println("Failed to read the metadata ", err)
-		return MetaData{}, err
-	}
-
-	return metadata, nil
 }
 
 func fetchHashAndValidate(respBody string, shaPartOfURL string) (string, error) { //able to find rthe sha256 hash
@@ -153,17 +163,13 @@ func fetchHashAndValidate(respBody string, shaPartOfURL string) (string, error) 
 	h.Write([]byte(respBody))
 	hashedval := h.Sum(nil)
 
-	fmt.Printf("%x", hashedval)
+	//fmt.Printf("%x", hashedval)
 	sha256Hash := "0x" + fmt.Sprintf("%x", hashedval)
-	//	fmt.Println("")
-	//fmt.Println("sha256Hash ", sha256Hash)
 
 	if shaPartOfURL == sha256Hash {
-		//	fmt.Println("Validation passed")
 		return "Validation passed", nil
 	} else {
 		err := errors.New("Hashes did not matched")
-		//	fmt.Println("Hashes did not passed")
 		return "Hashes did not matched", err
 	}
 
